15dc18571d30e64200a8698b56c98dea2ef1b95d
Meetra <77251614+MeetraYarhu@users.noreply.github.com>
2021-03-18 20:19:31 -0700

    relay.js: Made adjustments to how all json files are processed to shorten code and improve functionality

--- legacy/v2/commands/relay/relay.js
+++ legacy/v2/commands/relay/relay.js
@@ -6,7 +6,7 @@ const {
 } = require('discord.js');
 const fs = require('fs');
 const path = require('path');
-const locations = require('@util/locations.json');
+const allLocations = require('@util/locations.json');
 const allWorlds = require('@util/worlds.json');
 const allSpeeds = require('@util/speeds.json');
 const guildStuff = require('@util/guildstuff.json');
@@ -26,11 +26,6 @@ module.exports = class sendMessageCommand extends Command {
     }
     run(message, args) {
 
-        const allGuildName = guildStuff.map(obj => obj.guild[0].name);
-        const allGuildID = guildStuff.map(obj => obj.guild[0].guildid);
-        const allChannelID = guildStuff.map(obj => obj.guild[0].channelid);
-        const allTrainRoleID = guildStuff.map(obj => obj.guild[0].trainroleid)
-        const commandCenterID = guildStuff.map(obj => obj.guild[0].commandcenter)
         const guildList = this.client.guilds.cache.array()
 
         if (args.length !== 3) { // send appropriate error if arguments are not sufficient length
@@ -48,31 +43,18 @@ module.exports = class sendMessageCommand extends Command {
             const locationName = args[1].toLowerCase();
             const trainSpeed = args[2].toLowerCase();
 
-            // Mapping objects from locations.json to local arrays
-            const aetheryteNames = locations.map(obj => obj.aetheryte[0].name);
-            const aetheryteAliases = locations.map(obj => obj.aetheryte[0].aliases);
-            const aetheryteAllAliases = [];
-
-            // change this so the locations.json can just match lower case and delete these variables
-            const locFirstChar = locationName.charAt(0).toUpperCase();
-            const locRestChar = locationName.slice(1);
-            const locTitle = locFirstChar.concat(locRestChar);
-
-            // Mega array of all aetheryte aliases
-            for (let i = 0; i < aetheryteAliases.length; i++) {
-                aetheryteAllAliases.push(...aetheryteAliases[i]);
-            }
+            // Locate location configuration from alias input
+            const locationSelection = allLocations.name.find(obj => obj.aliases.includes(locationName));
 
             // Locate speed configuration from alias input
             const speedSelection = allSpeeds.speeds.find(obj => obj.aliases.includes(trainSpeed));
 
+            // Locate world configuration from alias input
+            const worldSelection = allWorlds.worlds.find(obj => obj.shorthand === worldInputSubstr)
+
             // check new local world arrays for substring, match index and set name
             const worldInputSubstr = worldName.substr(0, 3);
-            if (!aetheryteAllAliases.includes(locTitle) && !speedSelection) {
-                let errorReply = `Invalid aetheryte and speed, ${message.author}!`;
-                errorReply += '\nThe proper usage would be: `~relay <world> <aetheryte> <speed>`';
-                message.channel.send(errorReply);
-            } else if (!aetheryteAllAliases.includes(locTitle)) {
+            if (!aetheryteAllAliases) {
                 let errorReply = `Invalid aetheryte, ${message.author}!`;
                 errorReply += '\nThe proper usage would be: `~relay <world> <aetheryte> <speed>`';
                 message.channel.send(errorReply);
@@ -82,25 +64,30 @@ module.exports = class sendMessageCommand extends Command {
                 message.channel.send(errorReply);
             } else {
 
-                const worldSelection = allWorlds.worlds.find(obj => obj.shorthand === worldInputSubstr)
+
 
                 // Embed that will be used to confirm, then sent as the relay
                 const relayEmbed = new Discord.MessageEmbed()
                     .setColor('006CFF')
                     .setTitle('ShB hunt train starting!')
                     .setAuthor(`Relayed by: ${message.member.displayName}`, message.author.avatarURL())
-                    .setDescription(`Relay sent from ${message.guild.name}.`)
+                    .setDescription(`Relay sent from the ${message.guild.name} discord.`)
                     .addFields({
                         name: ':earth_americas: World',
                         value: worldSelection.name,
                         inline: true,
                     }, {
+                        name: 'Location',
+                        value: locationSelection.name,
+                        inline: true,
+                    }, {
                         name: 'Speed',
                         value: speedSelection.description,
                         inline: true,
-                    },)
-                    .setImage('')
-                    .setFooter(`Join the Tempo Bot discord: https://discord.gg/zusBKtp`);
+                    }, )
+                    .attachFiles(`/images/shbAetherytes/${locationSelection.filename}`)
+                    .setImage(`attachment://${locationSelection.filename}`)
+                    .setFooter(`Tempo Bot discord: https://discord.gg/zusBKtp`);
 
                 // Change embed color based on world
                 let colorDict = {
@@ -116,35 +103,11 @@ module.exports = class sendMessageCommand extends Command {
                 let color = colorDict[worldInputSubstr]
                 relayEmbed.setColor(color)
 
-                const aetheryteImages = fs.readdirSync('images/shbAetherytes');
-
-                // Loop through aetherytes to find matching index
-                for (let i = 0; i < aetheryteAliases.length; i++) {
-                    for (let k = 0; k < aetheryteAliases[i].length; k++) {
-                        if (aetheryteAliases[i][k].includes(locTitle)) {
-                            const tempIndexName = aetheryteAliases[i][0];
-                            const tempIndex = aetheryteNames.indexOf(tempIndexName);
-                            const chosenFile = aetheryteImages[tempIndex];
-                            const rid = 'shbAetherytes/' + chosenFile;
-                            const ridof = rid.replace('shbAetherytes/', '');
-
-                            relayEmbed.attachFiles(`images/shbAetherytes/${ridof}`);
-                            relayEmbed.addFields({
-                                name: 'Location',
-                                value: tempIndexName,
-                                inline: true,
-                            });
-                            relayEmbed.setImage(`attachment://${ridof}`);
-                            break;
-                        }
-                    }
-                }
-                // loop through speed to find matching name, description, and roleid
-                for (let i = 0; i < allGuildName.length; i++) {
-                    this.client.channels.cache.get(allChannelID[i]).send(`<@&${allTrainRoleID[i]}> ${worldSelection.roleid[i]} ${speedSelection.roleid}`, {
+                for (let i = 0; i < guildList.length; i++) {
+                    this.client.channels.cache.get(guildStuff[i].channelid).send(`<@&${guildStuff[i].trainroleid}> ${worldSelection.roleid[i]} ${speedSelection.roleid}`, {
                         embed: relayEmbed,
                     });
-                    this.client.channels.cache.get(commandCenterID[i]).send(`An ShB train on ${worldSelection.name} was relayed by ${message.author.username} from the ${message.guild.name} Discord.`);
+                    this.client.channels.cache.get(guildStuff[i].commandcenter).send(`An ShB train on ${worldSelection.name} was relayed by ${message.author.username} from the ${message.guild.name} Discord.`);
                 }
             }
         }
