9e28f3865cba3f8ba38775eec94765bb2ebb9f61
MeetraYarhu <megan02west@gmail.com>
2023-03-24 20:07:24 -0700

    Initial Commit

---
+++ src/commands/excal.js
@@ -0,0 +1,44 @@
+const { Command } = require('@sapphire/framework');
+
+class GiveListCommand extends Command {
+	constructor(context, options) {
+		super(context, {
+			...options,
+			name: 'excal',
+			description: 'Give Excal List role',
+			requiredClientPermissions: ['ManageRoles'],
+		});
+	}
+
+	registerApplicationCommands(registry) {
+		registry.registerChatInputCommand((builder) =>
+			builder
+				.setName('excal')
+				.setDescription('Give role to user')
+				.addUserOption(option =>
+					option
+						.setName('target')
+						.setDescription('user to give role to')
+						.setRequired(true)),
+		);
+	}
+
+	async chatInputRun(interaction) {
+
+		try {
+			const role = '1088609642826973204';
+			const member = await interaction.options.getMember('target');
+			const user = await interaction.options.getUser('target');
+
+			await member.roles.add(role);
+			await interaction.reply(`Added <@&${role}> to <@${user.id}>`);
+		}
+		catch (error) {
+			interaction.reply('Failed');
+		}
+	}
+}
+
+module.exports = {
+	GiveListCommand,
+};
