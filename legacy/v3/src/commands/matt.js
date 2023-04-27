b4028272b02beeee2b2aaef122a787ab6b62390c
MeetraYarhu <megan02west@gmail.com>
2023-04-27 14:29:46 -0700

    Initial Commit

---
+++ legacy/v3/src/commands/matt.js
@@ -0,0 +1,35 @@
+const { Command } = require('@sapphire/framework');
+const fs = require('fs');
+const path = require('path');
+const { AttachmentBuilder } = require('discord.js');
+
+class MattCommand extends Command {
+	constructor(context, options) {
+		super(context, {
+			...options,
+			name: 'matt',
+			aliases: ['matthew', 'chaser', 'greene'],
+			description: 'Posts a Matt meme.',
+		});
+	}
+
+	messageRun(message) {
+		const files = fs.readdirSync('images/mattpictures');
+
+		const chosenFile = files[Math.floor(Math.random() * files.length)];
+
+		const image = fs.readFileSync(path.join('images/mattpictures', chosenFile));
+
+
+		const attachment = new AttachmentBuilder(image, { name: chosenFile });
+
+		message.channel.send({
+			files: [attachment],
+		});
+
+	}
+}
+
+module.exports = {
+	MattCommand,
+};
