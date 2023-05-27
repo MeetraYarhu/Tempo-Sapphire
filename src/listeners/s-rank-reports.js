7447c682cd4d49d5e801236fe56e8f535776e4d2
MeetraYarhu <megan02west@gmail.com>
2023-05-27 11:03:48 -0700

    S-Rank-Reports working

--- src/listeners/s-rank-reports.js
+++ src/listeners/s-rank-reports.js
@@ -1,20 +1,60 @@
-const { Listener } = require('@sapphire/framework');
+const { Listener, CommandOptionsRunTypeEnum } = require('@sapphire/framework');
+const { EmbedBuilder } = require('discord.js');
 
 class ReportsListener extends Listener {
 	constructor(context, options) {
 		super(context, {
 			...options,
-			name: 'reports',
-			once: true,
+			once: false,
+			runIn: CommandOptionsRunTypeEnum.GuildText,
 			requiredClientPermissions: ['ReadMessageHistory', 'ManageMessages'],
-			event: 'report',
+			event: 'messageCreate',
 		});
 	}
 
-	run(message) {
+	async run(message) {
 
+		if ((message.channelId === '1101248697280495637') && (message.author.bot === false)) {
+
+			try {
+				const reportsChannel = message.guild.channels.cache.get('1101248716305862656');
+				const mentionRole = '1091974796532785212';
+				// Create Relay Embed
+				const relayEmbed = await new EmbedBuilder()
+					.setColor('Red')
+					.setAuthor({
+						name: message.author.username,
+						iconURL: message.author.avatarURL(),
+					})
+					.addFields({
+						name: `Sent a relay from <#${message.channel.id}>:`,
+						value: message.content,
+					})
+					.setTimestamp()
+					.setFooter({
+						text: `ID: ${message.author.id}`,
+					});
+
+				// Send final embed
+				await reportsChannel.send({
+					content: `<@&${mentionRole}>`,
+					embeds: [relayEmbed],
+					allowedMentions: {
+						roles: [mentionRole],
+					},
+				});
+
+				// Delete original message
+				await message.delete().catch(console.error);
+			}
+			catch (error) {
+				console.log(error);
+			}
+		}
+		else {
+			// Do Nothing
+		}
 	}
-
 }
 
 module.exports = {
