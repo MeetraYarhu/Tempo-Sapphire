f4b1f4534ce2dc79ee2c9ee5108c64990881af4f
MeetraYarhu <megan02west@gmail.com>
2024-01-18 16:52:35 -0700

    Fixed common errors

--- src/listeners/s-rank-reports.js
+++ src/listeners/s-rank-reports.js
@@ -13,16 +13,29 @@ class ReportsListener extends Listener {
 	}
 
 	async run(message) {
-		// coeurl 1101248423249846372
+
+		// Coeurl #s-rank-reports ID: 1101248423249846372
+		// bot testing #s-rank-reports ID: 1101248697280495637
 		if ((message.channelId === '1101248423249846372') && (message.author.bot === false)) {
 			// console.log(message.author);
+			// console.log(message);
+
 			try {
+
+				// Delete message and prevent errors for blank msgs/sticker
+				if (message.content === '') {
+					message.delete().catch(console.error);
+					console.log('ERROR: Blank Message or Sticker, successfully deleted');
+					return;
+				}
+
 				const reportsChannel = await message.guild.channels.cache.get('1101248388483272834');
 				const mainChannel = await message.guild.channels.cache.get('1101248423249846372');
 				const mentionRole = '1121306416389554266';
-				// coeurl 1101248388483272834
+				// Coeurl #manual-reports ID: 1101248388483272834
+				// bot testing #manual-reports ID: 1101248716305862656
 
-				// Create Relay Embed
+				// Create relay embed
 				const relayEmbed = await new EmbedBuilder()
 					.setColor('Red')
 					.setDescription(message.content)
@@ -52,7 +65,7 @@ class ReportsListener extends Listener {
 				const responseEmbed = await new EmbedBuilder()
 					.setColor('Green')
 					.setDescription(`Thanks for the report <@${message.author.id}>! I've relayed it to our spawners for checking.`);
-
+				// Set response delete time
 				await mainChannel.send({
 					embeds: [responseEmbed],
 				})
@@ -60,10 +73,30 @@ class ReportsListener extends Listener {
 						setTimeout(() => msg.delete(), 7500);
 					});
 
-				// Delete original message + Response to user
+				// Wait
 				await sleep(7500);
-				await message.delete().catch(console.error);
+
+				// Delete original message + error logs
+				await mainChannel.messages
+					.fetch(message.id)
+					.then((fetchedMessage) => {
+						console.log('Message exists');
+						fetchedMessage.delete()
+							.then(() => console.log('Message deleted successfully'))
+							.catch((err) => console.log('Could not delete message ----', err));
+					}).catch((err) => {
+						if (err.status === 404) {
+							console.log('ERROR: Message already deleted ----\n', err);
+						}
+						else if (err.code === '10008') {
+							console.log('ERROR: Message already deleted ----\n', err);
+						}
+						else {
+							console.log('ERROR: Unknown error ----\n', err);
+						}
+					});
 			}
+
 			catch (error) {
 				console.log(error);
 			}
