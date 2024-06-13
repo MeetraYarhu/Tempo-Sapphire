c220fc706428412556afbdb48b5b100b58e1d275
Meetra <megan02west@gmail.com>
2024-06-13 16:27:31 -0700

    Checknitro.js Initial working commit

---
+++ legacy/v3/src/listeners/checknitro.js
@@ -0,0 +1,56 @@
+const { Listener } = require('@sapphire/framework');
+// const idvariables = require('../util/idVariables.json');
+// const getMemberRoles = require('../util/getMemberRoles.js');
+const removeSpecificRoles = require('../util/removeSpecificRoles.js');
+
+class NitroListener extends Listener {
+	constructor(context, options) {
+		super(context, {
+			...options,
+			once: false,
+			requiredClientPermissions: ['ViewAuditLog', 'ManageRoles'],
+			event: 'guildAuditLogEntryCreate',
+		});
+	}
+
+	async run(event) {
+
+		// Returns $add or $remove
+		const [{ key }] = event.changes;
+
+		// ID of user being changed
+		const targetId = event.target.id;
+
+		// Coeurl GuildId = 481478007932846100
+		const guildId = '481478007932846100';
+
+		//  Returns members' roles by id and name
+		// const roles = await getMemberRoles(guildId, targetId);
+
+		// Returns name and id of the role added/removed
+		const [{ id: roleChangedId }] = event.changes[0].new;
+
+		const rolesToRemove = [
+			{ id: '827108756893990932', name: 'train' },
+			{ id: '864557267265257503', name: 'reporter' },
+			{ id: '823367370780966983', name: 'purple' },
+			{ id: '823368359725760552', name: 'white' },
+			{ id: '823368119812096021', name: 'green' },
+			{ id: '823366921453305876', name: 'orange' },
+			{ id: '823365226467950643', name: 'pink' },
+		];
+		const Nitro = '589366331074150405';
+
+		if ((event.action === 25) && (key === '$remove') && (roleChangedId === Nitro)) {
+			console.log(`${event.target.username}'s Nitro boost has expired.`);
+			await removeSpecificRoles(guildId, targetId, rolesToRemove);
+		}
+		else {
+			// do nothing
+		}
+	}
+
+}
+module.exports = {
+	NitroListener,
+};
