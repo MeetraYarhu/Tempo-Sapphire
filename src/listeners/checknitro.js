9088797ada73edfb22b86cb39dddcc0a2f53b556
Megan West <megan02west@gmail.com>
2025-01-10 12:41:56 -0700

    fixes

--- src/listeners/checknitro.js
+++ src/listeners/checknitro.js
@@ -18,7 +18,7 @@ class NitroListener extends Listener {
 		try {
 
 			if (Array.isArray(event.changes) && event.changes.length > 0) {
-
+				// console.log(event);
 				// Returns $add or $remove
 				const [{ key }] = event.changes;
 
@@ -26,6 +26,7 @@ class NitroListener extends Listener {
 				const targetId = event.target.id;
 
 				// Coeurl GuildId = 481478007932846100
+				// Testing GuildID = 948851726591614986
 				const guildId = '481478007932846100';
 
 				//  Returns members' roles by id and name
@@ -33,16 +34,26 @@ class NitroListener extends Listener {
 
 				// Returns name and id of the role added/removed
 				const [{ id: roleChangedId }] = event.changes[0].new;
+				// Coeurl Roles:
 				const rolesToRemove = [
-					{ id: '827108756893990932', name: 'train' },
-					{ id: '864557267265257503', name: 'reporter' },
 					{ id: '823367370780966983', name: 'purple' },
 					{ id: '823368359725760552', name: 'white' },
 					{ id: '823368119812096021', name: 'green' },
 					{ id: '823366921453305876', name: 'orange' },
 					{ id: '823365226467950643', name: 'pink' },
 				];
-				const Nitro = '589366331074150405';
+					const Nitro = '589366331074150405';
+
+				// Testing Server Roles:
+				/* const rolesToRemove = [
+					{ id: '1091974696540569641', name: 'purple' },
+					{ id: '1091974727721029673', name: 'white' },
+					{ id: '1091974745689428019', name: 'green' },
+					{ id: '1091974758259752980', name: 'orange' },
+					{ id: '1091974772738502746', name: 'pink' },
+				];
+					const Nitro = '1250572918081982464'; */
+				
 
 				if ((event.action === 25) && (key === '$remove') && (roleChangedId === Nitro)) {
 					console.log(`${event.target.username}'s Nitro boost has expired.`);
