16adc45cfc45725e0a4a4e9f81ce465ec964c754
Megan West <megan02west@gmail.com>
2025-01-24 13:41:12 -0700

    improved variables, added filtering

--- legacy/v3/src/listeners/checknitro.js
+++ legacy/v3/src/listeners/checknitro.js
@@ -27,38 +27,28 @@ class NitroListener extends Listener {
 				const targetId = event.target.id;
 
 				// Define each guild as it's own object
-				const idvars = (idvariables.coeurl);
-				// const idvars = (idvariables.tempotesting);
+				// const idvars = await (idvariables.coeurl);
+				const idvars = await (idvariables.tempotesting);
 
-				// Coeurl GuildId = 481478007932846100
-				// Testing GuildID = 948851726591614986
-				const guildId = idvars.guild.id;
+				const guildId = await idvars.guild.id;
 
 				//  Returns members' roles by id and name
 				// const roles = await getMemberRoles(guildId, targetId);
 
 				// Returns name and id of the role added/removed
 				const [{ id: roleChangedId }] = event.changes[0].new;
-				// Coeurl Roles:
-				const rolesToRemove = [
-					{ id: '823367370780966983', name: 'purple' },
-					{ id: '823368359725760552', name: 'white' },
-					{ id: '823368119812096021', name: 'green' },
-					{ id: '823366921453305876', name: 'orange' },
-					{ id: '823365226467950643', name: 'pink' },
-				];
-				const Nitro = '589366331074150405';
 
-				// Testing Server Roles:
-				/* const rolesToRemove = [
-					{ id: '1091974696540569641', name: 'purple' },
-					{ id: '1091974727721029673', name: 'white' },
-					{ id: '1091974745689428019', name: 'green' },
-					{ id: '1091974758259752980', name: 'orange' },
-					{ id: '1091974772738502746', name: 'pink' },
-				];
-				const Nitro = '1250572918081982464'; */
+				// Automatically generate rolesToRemove using the labels and IDs
+				const rolesToRemove = Object.keys(idvars.roles)
 
+				// Filter keys that start with "nitro", and excludes the nitro role itself
+					.filter((keyrole) => keyrole.startsWith('nitro') && keyrole !== 'nitro')
+					.map((keyrole) => ({
+						id: idvars.roles[keyrole],
+						// Remove "nitro" from the key to get the color
+						name: keyrole.replace('nitro', ''),
+					}));
+				const Nitro = idvars.roles.nitro;
 
 				if ((event.action === 25) && (key === '$remove') && (roleChangedId === Nitro)) {
 					console.log(`Checknitro.js: ${event.target.username}'s Nitro boost has expired.`);
