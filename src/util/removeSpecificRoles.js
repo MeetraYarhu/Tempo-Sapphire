5be54752eb582b70026f11c95702d679a327d3d2
MeetraYarhu <megan02west@gmail.com>
2026-03-03 16:03:55 -0700

    restructure to resolve role within the function

--- src/util/removeSpecificRoles.js
+++ src/util/removeSpecificRoles.js
@@ -4,38 +4,39 @@ const getCallerModule = require('@util/getCallerModule.js');
 async function removeSpecificRoles(guildId, memberId, rolesToRemove) {
 	const callerModule = getCallerModule();
 
-	if (!Array.isArray(rolesToRemove)) {
-		console.log(`${callerModule}: rolesToRemove must be an array`);
-		throw new Error('rolesToRemove must be an array');
-	}
+	if (!Array.isArray(rolesToRemove)) throw new Error('rolesToRemove must be an array');
 
 	try {
 		const guild = await client.guilds.fetch(guildId);
-		if (!guild) {
-			console.log(`${callerModule}: Guild not found`);
-			throw new Error('Guild not found');
-		}
-
 		const member = await guild.members.fetch(memberId);
-		if (!member) {
-			console.log(`${callerModule}: Member not found`);
-			throw new Error('Member not found');
-		}
-
 		const memberName = member.user.tag;
 
-		for (const role of rolesToRemove) {
-			const roleId = role.id;
+		for (const roleId of rolesToRemove) {
+			if (typeof roleId !== 'string' || !roleId) {
+				console.log(`${callerModule}: Invalid role ID - ${roleId}`);
+				continue;
+			}
+
+			const role = guild.roles.cache.get(roleId) ??
+			await guild.roles.fetch(roleId).catch(() => null);
+
+			if (!role) {
+				console.log(`${callerModule}: Role not found - ID: ${roleId}`);
+				continue;
+			}
+
 			const roleName = role.name;
 
-			if (member.roles.cache.has(roleId)) {
-				await member.roles.remove(roleId);
-				console.log(`${callerModule}: Removed role ${roleName} (ID: ${roleId}) from member ${memberName} (ID: ${memberId})`);
+			if (!member.roles.cache.has(roleId)) {
+				console.log(`${callerModule}: Member does not have role - ${roleName} (ID: ${roleId})`);
+				continue;
 			}
+
+			await member.roles.remove(roleId);
+			console.log(`${callerModule}: Removed role ${roleName} (ID: ${roleId}) from member ${memberName} (ID: ${memberId})`);
 		}
-	}
-	catch (error) {
-		console.error('Error managing member roles:', error);
+	}	catch (error) {
+		console.error(`${callerModule}: Error managing member roles:`, error);
 	}
 }
 
